24c84dbf013fbb001ca6eaedb9f0dc04cb50c50b
youssefashraf-1 <n4pyyc6r2b@privaterelay.appleid.com>
2025-11-11 09:34:12 +0200

    completed
    Implemented Person class with constructors, getters, and setters.

--- Person.h
+++ Person.h
@@ -2,19 +2,37 @@
 #define PERSON_H
 
 #include <string>
+#include <iostream>
 using namespace std;
+class Person {
+private:
+    std::string name;
+    int id;
 
-//#write Person class here
-
-
-
-
-
-
-
-
+public:
+    Person() : name(""), id(0) {}
+    Person(string name, int id) : name(std::move(name)), id(id) {}
 
 
+    const string& getName()
+    const {
+        return name;
+    }
+    int getId()
+    const {
+        return id;
+    }
 
+ 
+    void setName(const string& n) {
+        name = n;
+    }
+    void setId(int i) {
+        id = i;
+    }
+
+   
+    void display();
+};
 
 #endif
