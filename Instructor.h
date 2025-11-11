9273e57dd299715a5f1896641d5a7a9672d023f2
youssefashraf-1 <n4pyyc6r2b@privaterelay.appleid.com>
2025-11-11 09:33:31 +0200

    completed

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,29 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
 
+#include <string>
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
 
+public:
+    Instructor() : department(""), experienceYears(0) {}
+    Instructor(string name, int id, string department, int experienceYears)
+    : Person(std::move(name), id),
+      department(std::move(department)),
+      experienceYears(experienceYears) {}
 
+    const string& getDepartment() const { return department; }
+    int getExperienceYears() const { return experienceYears; }
 
+    void setDepartment(string d) { department = std::move(d); }
+    void setExperienceYears(int y) { experienceYears = y; }
 
+    void display();
+};
 
+#endif // INSTRUCTOR_H
 
-
-
-
-
-
-
-#endif
