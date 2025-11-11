2dbad604e117bf065401fe651e31395f9b1eee66
youssefashraf-1 <n4pyyc6r2b@privaterelay.appleid.com>
2025-11-11 09:35:40 +0200

    Implement Student class with attributes and methods
    Added Student class that inherits from Person with year level and major attributes.

--- Student.h
+++ Student.h
@@ -1,21 +1,26 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include "Person.h"
 #include <string>
-#using namespace std;
-//#write student class here
+#include "Person.h"
 
+class Student : public Person {
+private:
+    int yearlevel;
+    std::string major;
 
+public:
+    Student() : yearlevel(0), major("") {}
+    Student(std::string name, int id, int yearlevel, std::string major)
+    : Person(std::move(name), id), yearlevel(yearlevel), major(std::move(major)) {}
 
+    int getYearLevel() const { return yearlevel; }
+    const std::string& getMajor() const { return major; }
 
+    void setYearLevel(int yl) { yearlevel = yl; }
+    void setMajor(std::string m) { major = std::move(m); }
 
-
-
-
-
-
-
-
+    void display();
+};
 
 #endif
